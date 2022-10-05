f7800073e1a98015574111801a657b597888fd24
Michael <michael.w.kolb@gmail.com>
2022-10-05 15:37:38 -0400

    Completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
 # Your code goes here!
+
+class Anagram
+    attr_accessor :word, :array
+
+    def initialize(word)
+        @word=word
+    end
+
+    def match(array)
+        temp_array = []
+        @array=array 
+        @array.map do |item|
+            puts item
+            item_letters = item.split('').sort.to_s
+            word_letters = @word.split('').sort.to_s
+            if item_letters == word_letters
+                temp_array << "#{item}"
+            end
+        end
+        if temp_array != []
+            return temp_array
+        else
+            return []
+        end
+    end
+
+end
